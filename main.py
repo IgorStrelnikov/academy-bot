f9e9797bb531d6ec79bd530b865d6d79fdf3a7c9
IgorStrelnikov <2578577@gmail.com>
2025-04-17 15:52:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import logging
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
-from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
